ab95e687ae72e1a8b38bde3e60a7f6985fc485ca
kritsakorn choatchaojaru <kritsakorn.cho@axonstech.com>
2024-01-06 08:13:07 +0700

    init dot files
    # Conflicts:
    #	after/plugin/lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -24,6 +24,41 @@ lsp.configure('lua-language-server', {
     }
 })
 
+require('sonarlint').setup({
+   server = {
+      cmd = {
+         'sonarlint-language-server',
+         -- Ensure that sonarlint-language-server uses stdio channel
+         '-stdio',
+         '-analyzers',
+         -- paths to the analyzers you need, using those for python and java in this example
+         vim.fn.expand("/Users/means/.local/share/sonarlint-analyzers/sonarjs.jar"),
+         vim.fn.expand("/Users/means/.local/share/sonarlint-analyzers/sonarpython.jar"),
+         vim.fn.expand("/Users/means/.local//share/sonarlint-analyzers/sonarcfamily.jar"),
+         vim.fn.expand("/Users/means/.local//share/sonarlint-analyzers/sonarjava.jar"),
+      },
+      -- All settings are optional
+      settings = {
+         -- The default for sonarlint is {}, this is just an example
+         sonarlint = {
+            rules = {
+               ['typescript:S101'] = { level = 'on', parameters = { format = '^[A-Z][a-zA-Z0-9]*$' } },
+               ['typescript:S103'] = { level = 'on', parameters = { maximumLineLength = 180 } },
+               ['typescript:S106'] = { level = 'on' },
+               ['typescript:S107'] = { level = 'on', parameters = { maximumFunctionParameters = 7 } }
+            }
+         }
+      }
+   },
+   filetypes = {
+      -- Tested and working
+      'python',
+      'cpp',
+			'typescript',
+			'java',
+   }
+})
+
 local on_attach = function(client, bufnr)
   -- format on save
   if client.server_capabilities.documentFormattingProvider then
